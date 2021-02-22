0ac93ac8edb20b262ecfefc79d82a13fc358931b
Giovanni Amormino <gigio_jr@hotmail.com>
2021-02-22 17:38:38 -0300

    Create application

---
+++ app/src/main/java/com/pokemon/PokemonApp.kt
@@ -0,0 +1,12 @@
+package com.pokemon
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokemonApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
