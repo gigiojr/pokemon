63149605232e9749ba8b107fdae7dd03ee7ceb2a
Giovanni Amormino <gigio_jr@hotmail.com>
2021-02-23 10:29:31 -0300

    Update FirstFragment layout to use components

--- app/src/main/java/com/pokemon/ui/FirstFragment.kt
+++ app/src/main/java/com/pokemon/ui/FirstFragment.kt
@@ -1,27 +1,33 @@
 package com.pokemon.ui
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.pokemon.R
 import com.pokemon.data.model.api.Pokemon
 import com.pokemon.data.viewmodel.MainViewModel
 import com.pokemon.databinding.FragmentFirstBinding
+import com.pokemon.ui.component.PokemonFoundComponent
+import com.pokemon.ui.component.SearchBarComponent
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), PokemonFoundComponent.Listener, SearchBarComponent.Listener {
 
     private lateinit var binding: FragmentFirstBinding
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
 
         binding = FragmentFirstBinding.inflate(layoutInflater)
@@ -35,24 +41,37 @@ class FirstFragment : Fragment() {
     }
 
     private fun initView() {
-        binding.btSearch.setOnClickListener { onSearch(it) }
+        hideLoad()
+        binding.pokemonComponent.visibility = View.GONE
+        binding.searchBarComponent.listener = this
+
         viewModel.isLoading.observe(viewLifecycleOwner, { if (it) showLoad() else hideLoad() })
         viewModel.pokemonFound.observe(viewLifecycleOwner, { foundPokemon(it) })
     }
 
-    private fun onSearch(view: View) {
-        viewModel.getPokemon(binding.searchField.text.toString())
+    override fun onSearchClick(text: String) {
+        onSearch(text)
+    }
+
+    private fun onSearch(text: String) {
+        viewModel.getPokemon(text)
     }
 
     private fun foundPokemon(pokemon: Pokemon) {
-        Toast.makeText(context, pokemon.name, Toast.LENGTH_LONG).show()
+        binding.pokemonComponent.showPokemon(pokemon)
+        binding.pokemonComponent.visibility = View.VISIBLE
+        binding.searchBarComponent.clearField()
+    }
+
+    override fun onCatchClick(pokemon: Pokemon) {
+
     }
 
     private fun showLoad() {
-
+        binding.searchBarComponent.showLoad()
     }
 
     private fun hideLoad() {
-
+        binding.searchBarComponent.hideLoad()
     }
 }
