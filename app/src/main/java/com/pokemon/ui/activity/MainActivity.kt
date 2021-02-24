315a8f8cd16df00fd9a2c5e858fc63cc4c7f7a6b
Giovanni Amormino <gigio_jr@hotmail.com>
2021-02-24 16:15:36 -0300

    Update activity and fragment folders

---
+++ app/src/main/java/com/pokemon/ui/activity/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.pokemon.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pokemon.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
