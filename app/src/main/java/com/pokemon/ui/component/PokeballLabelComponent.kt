fb0ff5976434208aedc35743b722c7b16a0f85a6
Giovanni Amormino <gigio_jr@hotmail.com>
2021-02-25 18:12:46 -0300

    Create PokeballLabelComponent

---
+++ app/src/main/java/com/pokemon/ui/component/PokeballLabelComponent.kt
@@ -0,0 +1,40 @@
+package com.pokemon.ui.component
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.pokemon.R
+import com.pokemon.databinding.ComponentPokeballLabelBinding
+
+class PokeballLabelComponent(context: Context, attrs: AttributeSet?)
+    : ConstraintLayout(context, attrs), View.OnClickListener {
+
+    private var viewLayout = View.inflate(context, R.layout.component_pokeball_label, this)
+    private var binding = ComponentPokeballLabelBinding.bind(viewLayout)
+
+    var id: Int? = null
+    var text: String? = null
+        set(value) {
+            binding.label.text = value ?: ""
+            field = value
+        }
+
+    var listener: Listener? = null
+
+    init {
+        initComponent()
+    }
+
+    private fun initComponent() {
+        binding.pokeballLabelLayout.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        listener?.onClick(id, text)
+    }
+
+    interface Listener {
+        fun onClick(id: Int?, text: String?)
+    }
+}
